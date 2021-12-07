0bdadd5af91b9c92a15867e50683344cbf8d7cf8
Lázaro Pimentel <lazbonfim@hotmail.com>
2021-12-07 04:52:07 -0300

    Home section

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .navbar');
+let menu = document.querySelector('#menu-btn')
+let navbar = document.querySelector('.header .navbar')
 
 menu.onclick = () => {
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
+  menu.classList.toggle('fa-times')
+  navbar.classList.toggle('active')
 }
+
+// função de Remover durante o rolamento
+window.onscroll = () => {
+  menu.classList.remove('fa-times')
+  navbar.classList.remove('active')
+}
+
+//função do efeito de swiper
+var swiper = new Swiper('.home-slider', {
+  speed: 800,  //Velocidade de transição de slide
+  effect: 'fade',
+  grabCursor: true,
+  loop:true,
+  centeredSlides: true,
+  autoplay: { //
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+});
