9888d5593b9709aa100114f47e237f1bf9e31f59
Lázaro Pimentel <lazbonfim@hotmail.com>
2021-12-09 00:32:23 -0300

    Blog Section

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ var swiper = new Swiper('.home-slider', {
 
 //função do efeito de swiper - testimonials
 var swiper = new Swiper('.review-slider', {
+  spaceBetween: 20,
   speed: 800,  //Velocidade de transição de slide
   grabCursor: true,
   loop:true,
@@ -38,5 +39,41 @@ var swiper = new Swiper('.review-slider', {
     delay: 3500,
     disableOnInteraction: false,
   },
+  breakpoints:{
+    0:{
+      slidesPerView:1,
+    },
+    600:{
+      slidesPerView:2,
+    }
+  }
 });
+
+// Efeito Carrousel no Blog
+var swiper = new Swiper('.blogs-slider', {
+  spaceBetween: 20,
+  speed: 800,  //Velocidade de transição de slide
+  grabCursor: true,
+  loop:true,
+  autoplay: { 
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints:{
+    0:{
+      slidesPerView:1,
+    },
+    600:{
+      slidesPerView:2,
+    },
+    1200:{
+      slidesPerView:3,
+    },
+  }
+});
+
 
